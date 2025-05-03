cef190c71708ae7f01b8b7a829bd91f2a3161154
o-ankomochi-o <guugurunomeeru@gmail.com>
2025-05-03 19:32:16 +0900

    KTOGenerationEvaluationTrainer

--- kto_generation_evaluation_trainer.py
+++ kto_generation_evaluation_trainer.py
@@ -16,7 +16,7 @@ from openai import OpenAI
 from trl import KTOTrainer
 
 
-class GenerationEvaluationTrainer(KTOTrainer):
+class KTOGenerationEvaluationTrainer(KTOTrainer):
     """
     OpenAI評価付きKTOトレーナー
 
