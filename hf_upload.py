57762d52542462b8feb7dac62ba53ddc1db396de
o-ankomochi-o <guugurunomeeru@gmail.com>
2025-05-17 17:12:33 +0900

    モデルのpath

--- hf_upload.py
+++ hf_upload.py
@@ -2,7 +2,7 @@ from huggingface_hub import HfApi
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="/path/to/local/model",
+    folder_path="/home/www_kyoko_ogawa/simpo_train_project/output/generation-evaluation-trainer",
     repo_id="collabo-m/DiverseGen-KTO",
     repo_type="model",
 )
