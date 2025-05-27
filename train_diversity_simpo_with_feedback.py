475705b777a7989458fce39aa1080301a81a7d12
o-ankomochi-o <guugurunomeeru@gmail.com>
2025-05-27 14:18:27 +0900

    データをフォーマットしなくていい

--- train_diversity_simpo_with_feedback.py
+++ train_diversity_simpo_with_feedback.py
@@ -187,10 +187,13 @@ def preprocess_function(example):
 
 
 print("Preprocessing dataset...")
-formatted_train_dataset = train_dataset.map(preprocess_function, batched=False)
-formatted_test_dataset = test_dataset.map(preprocess_function, batched=False)
-formatted_train_dataset = formatted_train_dataset.select(range(200))
-formatted_test_dataset = formatted_test_dataset.select(range(100))
+# formatted_train_dataset = train_dataset.map(preprocess_function, batched=False)
+# formatted_test_dataset = test_dataset.map(preprocess_function, batched=False)
+# formatted_train_dataset = formatted_train_dataset.select(range(200))
+# formatted_test_dataset = formatted_test_dataset.select(range(100))
+
+formatted_train_dataset = train_dataset
+formatted_test_dataset = test_dataset
 
 # Remove unnecessary columns
 columns_to_remove = list(
