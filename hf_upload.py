e0dd54b24aa1aab988e5411dadbbbe75e4ba4abd
o-ankomochi-o <guugurunomeeru@gmail.com>
2025-07-01 20:42:58 +0900

    hf_upload

--- hf_upload.py
+++ hf_upload.py
@@ -5,6 +5,6 @@ from huggingface_hub import HfApi
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="/home/www_kyoko_ogawa/simpo_train_project/output/generation-evaluation-trainer",
-    repo_id="collabo-m/elyza-llama3-jp8b-simpo-adfeedback",
+    repo_id="collabo-m/KTO_diverse",
     repo_type="model",
 )
